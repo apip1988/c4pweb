b3e287c37e779e3aac8ea51b628aa1b5173c9d8d
Muhammad Afif <afifnordin88@gmail.com>
2026-04-06 01:33:32 +0800

    Fix MassAssignmentException pada PhcalsResult

--- app/PhcalsResult.php
+++ app/PhcalsResult.php
@@ -6,5 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class PhcalsResult extends Model
 {
-    //
+    // Ini adalah 'kunci' untuk benarkan data masuk ke table
+    protected $fillable = [
+        'user_id',
+        'set_number',
+        'score',
+        'correct_count',
+        'start_time',
+        'end_time'
+    ];
+
+    /**
+     * Hubungkan dengan User (Optional tapi bagus untuk ada)
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
