581846e41ae64452f29f454605614c1165ad386c
Muhammad Afif <afifnordin88@gmail.com>
2026-04-06 12:44:24 +0800

    Bina table rujukan_documents

---
+++ database/migrations/2026_04_06_044337_create_rujukan_documents_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+{
+    Schema::create('rujukan_documents', function (Blueprint $table) {
+        $table->id();
+        $table->string('type');          // SPG, Surat, Guideline, Minit Mesyuarat
+        $table->string('title');         // Tajuk Dokumen
+        $table->string('publisher');     // Dikeluarkan oleh (MKM, KKM, dsb)
+        $table->year('year');            // Tahun
+        $table->string('file_path');     // Lokasi fail PDF
+        $table->timestamps();
+    });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rujukan_documents');
+    }
+};
