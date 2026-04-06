e645608cf72f7b71054aa7a8c2f8ec8962c95b4e
Muhammad Afif <afifnordin88@gmail.com>
2026-04-06 15:57:15 +0800

    Fix mass assignment on RujukanDocument model

--- app/RujukanDocument.php
+++ app/RujukanDocument.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class RujukanDocument extends Model
 {
-    //
+    // Ini adalah kunci untuk membenarkan semua kolum diisi (Mass Assignment)
+    protected $guarded = []; 
+
+    // ATAU Afif boleh senaraikan satu-persatu (Fillable)
+    // protected $fillable = ['type', 'title', 'publisher', 'year', 'file_path'];
 }
