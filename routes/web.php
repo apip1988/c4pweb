9fbc44303bb0cafbdcb16e50ab0382e4a7e7b321
Muhammad Afif <afifnordin88@gmail.com>
2026-04-13 01:29:20 +0800

    pengurusan calon

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
-| Web Routes - MASTER COPY AFIF (FIX SEMUA 404 & ROUTE NOT FOUND)
+| Web Routes - MASTER COPY AFIF (FIX TOTAL 404 SEMAKAN)
 |--------------------------------------------------------------------------
 */
 
@@ -23,20 +23,25 @@ Route::post('register', [App\Http\Controllers\Auth\RegisterController::class, 'r
 Route::post('logout', [App\Http\Controllers\Auth\LoginController::class, 'logout'])->name('logout');
 Route::get('password/reset', function() { return "Fungsi Reset Belum Aktif."; })->name('password.request');
 
-// --- 3. e-KOMPETENSI (USER & PROSES) ---
+// --- 3. e-KOMPETENSI (USER & PROSES DAFTAR) ---
 Route::middleware(['auth'])->group(function () {
     Route::get('/kompetensi/permohonan', [KompetensiController::class, 'borang_permohonan']);
     Route::post('/kompetensi/hantar', [KompetensiController::class, 'hantar_permohonan'])->name('kompetensi.hantar');
 });
 
-// SEMAKAN e-KOMPETENSI (Anti-404)
+// --- 4. SEMAKAN e-KOMPETENSI (PENYELAMAT 404) ---
+// Semak Tempat
 Route::get('/kompetensi/tempat', [KompetensiController::class, 'halaman_semak_tempat'])->name('kompetensi.tempat');
 Route::post('/kompetensi/proses-semak-tempat', [KompetensiController::class, 'proses_semak_tempat'])->name('kompetensi.proses_semak_tempat');
+
+// Semak Keputusan (INI YANG KAU KENA TADI)
 Route::get('/kompetensi/semak', [KompetensiController::class, 'user_index'])->name('kompetensi.semak');
+// Pastikan nama ini 'kompetensi.proses_semak' sebijik macam dalam borang <form action="...">
 Route::post('/kompetensi/proses-semak', [KompetensiController::class, 'proses_semak_keputusan'])->name('kompetensi.proses_semak');
+
 Route::get('/kompetensi/cetak-slip/{ic}', [KompetensiController::class, 'cetak_slip'])->name('kompetensi.cetak_slip');
 
-// --- 4. e-KOMPETENSI (ADMIN) ---
+// --- 5. e-KOMPETENSI (ADMIN) ---
 Route::middleware(['auth'])->group(function () {
     Route::get('/admin/kompetensi/pengurusan-calon', [KompetensiController::class, 'admin_pengurusan_calon'])->name('kompetensi.admin_pengurusan');
     Route::post('/admin/kompetensi/sahkan', [KompetensiController::class, 'sahkan_permohonan'])->name('kompetensi.sahkan');
@@ -45,16 +50,16 @@ Route::middleware(['auth'])->group(function () {
     Route::delete('/admin/kompetensi/delete/{id}', [KompetensiController::class, 'destroy'])->name('kompetensi.destroy');
 });
 
-// --- 5. e-PRPA ---
+// --- 6. e-PRPA ---
 Route::get('/prpa', function () { return view('prpa.index'); })->name('prpa.index');
 Route::get('/prpa/semak-keputusan', function () { return view('prpa.semak'); })->name('prpa.semak.borang');
 Route::post('/prpa/hasil-semakan', function () { return "Hasil PRPA"; })->name('prpa.semak.hasil');
 
-// --- 6. e-CREDENTIALING (PENYELAMAT ERROR KAU TADI) ---
+// --- 7. e-CREDENTIALING ---
 Route::get('/credentialing', function () { return view('credentialing.index'); })->name('credentialing.index');
 Route::get('/credentialing/create', function () { return view('credentialing.create'); })->name('credentialing.create');
 
-// --- 7. MENU DIREKTORI & LAIN-LAIN ---
+// --- 8. LAIN-LAIN ---
 Route::get('/direktori/carian-ppp', function () { return view('direktori.carian'); });
 Route::get('/direktori/carta-organisasi', function () { return view('direktori.carta'); })->name('direktori.carta-organisasi');
 Route::get('/rujukan', function () { return view('rujukan.index'); })->name('rujukan.index');
