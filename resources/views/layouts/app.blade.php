8e54d0e397c6bc040f9db04589a7c313cc5cc32e
Muhammad Afif <afifnordin88@gmail.com>
2026-04-14 15:49:25 +0800

    semak tempat kompetensi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -193,9 +193,7 @@
                                             <li><a href="{{ route('admin.users.index') }}"><i class="fas fa-users mr-2 text-dark"></i> Pengurusan pengguna</a></li>
                                         @endif
                                         
-                                        <li><a class="nav-link" href="{{ route('admin.dokumen.utama') }}">
-    <i class="fas fa-file-alt"></i> Pengurusan Dokumen
-</a></li>
+                                        <li><a href="{{ route('credentialing.index') }}"><i class="fas fa-envelope mr-2 text-success"></i> Pengurusan dokumen</a></li>
                                         <hr class="my-1">
                                     @endif
 
